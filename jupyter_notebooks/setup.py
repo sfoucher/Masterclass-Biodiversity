5a310fb2bd1c64ad0f3fd46070f71e1364a21b8c
sfoucher <samuel.foucher@gmail.com>
2021-12-01 11:36:55 -0500

    package name change

--- jupyter_notebooks/setup.py
+++ jupyter_notebooks/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='jupyter_notebooks',
+    name='MyPackageName',
     version='1.0.0',
     url='https://github.com/mypackage.git',
     author='Author Name',
